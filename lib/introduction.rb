8d31c06b826ca8838515d9ceeed493beba189bd0
Sandie Nunez <snunez@vt.edu>
2020-03-24 23:13:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,4 +4,4 @@ def introduction (sandie)
   puts "Hi, my name is #{sandie}."
 end
 def introduction_with_langauge(name,language)
-  puts "Hi, my name is #name{name"
+  puts "Hi, my name is #name{name} and I am learning to program in #{language}."
